02560e71dbbe587d482b604e2e347859be27cd6b
Michael Milette <michael.milette@tngconsulting.ca>
2017-10-20 15:18:57 -0400

    Backup v2.8.0.4 for WebsiteBaker.

---
+++ info.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *
+ * @category        modules
+ * @package         backup
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2010, Website Baker Org. e.V.
+ * @link            http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 4.3.4 and higher - See notes below for file backups.
+ * @version         $Id$
+ * @filesource      $HeadURL$
+ * @lastmodified    $Date$
+ *
+ */
+
+$module_directory	= 'backup';
+$module_name		= 'Backup';
+$module_function	= 'tool';
+$module_version		= '2.8.0.4';
+$module_platform	= '2.7 | 2.8.x';
+$module_author		= 'Ryan Djurovich, John, Hans>Null (QuickFix), Dietrich Roland Pehlke (aldus), File Backup (mjm4842).';
+$module_license		= 'GNU General Public License';
+$module_description = 'This module allows you to backup your database and your files.';
+$module_guid		= 'E3119CE9-FBD4-4AB0-A2D5-8B366B432F13';
+?>
